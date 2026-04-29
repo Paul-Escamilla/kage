ee03c5fed080f9af74b3ac651c06898f82f3dbcd
Paul Escamilla <178688683+Paul-Escamilla@users.noreply.github.com>
2026-04-29 18:14:51 +0000

    Optimizacion de funcion base_of_cage

--- src/kage/kage.py
+++ src/kage/kage.py
@@ -10,26 +10,26 @@ def base_of_cage(k, g):
         for i in range(1, int(g / 2)):
             nodos_nuevos = nodos + 2*(k - 1)**i
             for j in friends_down:
-                for l in range(k-1):
+                for _ in range(k-1):
                     G.add_edge(j, next_friend)
                     next_friend += 1
             friends_down = [amigos for amigos in range(nodos, nodos_nuevos)]
             nodos = nodos_nuevos   
         G.add_nodes_from(range(nodos))
     else:
-        nodos = 1
-        friends_down = [0]
-        next_friend = 1
-        
+        nodos = k + 1
+        friends_down = [i for i in range(1, k+1)]
+        next_friend = k + 1
+
+        for i in range(0, k + 1):
+            G.add_edge(0, i)
+
         for i in range(int((g - 1) / 2)):
             nodos_nuevos = nodos + k*(k - 1)**i
             for j in friends_down:
                 for l in range(k-1):
                     G.add_edge(j, next_friend)
                     next_friend += 1
-                if i == 0:
-                    G.add_edge(j, next_friend)
-                    next_friend +=1
             friends_down = [amigos for amigos in range(nodos, nodos_nuevos)]
             nodos = nodos_nuevos
         G.add_nodes_from(range(nodos))
